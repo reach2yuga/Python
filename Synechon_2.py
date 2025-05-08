b0b06ce89a69b7cbb8eac0eeae31bbb4e38305ab
reach2yuga <53001728+reach2yuga@users.noreply.github.com>
2025-05-08 11:37:43 +0000

    update

--- Synechon_2.py
+++ Synechon_2.py
@@ -1,2 +1,11 @@
-for i in range(0,3):
-    print("Hello world!")
+a = [1, 2, 3, 4, 5]
+
+def reverse_string(a):
+    final_list = []  # Initialize an empty list to store the reversed elements
+    for i in range(len(a) - 1, -1, -1):  # Loop from the last index to the first index
+        final_list.append(a[i])  # Append each element to final_list
+    return final_list  # Return the reversed list
+
+# Call the function and print the reversed list
+reversed_list = reverse_string(a)
+print("Reversed List:", reversed_list)
