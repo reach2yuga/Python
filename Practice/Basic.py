f3ae9c256e61b2ce23b9a0fb8675822cc7ecd957
reach2yuga <53001728+reach2yuga@users.noreply.github.com>
2025-06-05 14:42:34 +0000

    practice

---
+++ Practice/Basic.py
@@ -0,0 +1,22 @@
+#concate 2 lists
+a = [1,2,3]
+b = [4,5,6]
+c=a+b
+print(c)
+
+#add elements from first list to 2nd list
+
+a = [1,2,8]
+b = [4,5,6]
+a.extend(b)
+print(a)
+
+#for loop
+for i in range(5):
+    print(i)
+
+#while loop
+c = 0
+while c < 5:
+    print(c)
+    c += 1
